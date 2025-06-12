20a518a2a49f170e62a592f0598c031f6a6cf45e
kunhtml <phamxuanhieu04112000@gmail.com>
2025-06-13 00:52:34 +0700

    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -11,6 +11,7 @@ import {
 } from "react-bootstrap";
 import { toast } from "react-toastify";
 import { useAppContext } from "../context/AppContext";
+import formatPrice from "../utils/formatPrice";
 
 const Cart = () => {
   const navigate = useNavigate();
@@ -110,7 +111,7 @@ const Cart = () => {
                     <Col md={3}>
                       <Link to={`/products/${item.product || item.id}`}>{item.name}</Link>
                     </Col>
-                    <Col md={2}>${item.price}</Col>
+                    <Col md={2}>{formatPrice(item.price)}</Col>
                     <Col md={2}>
                       <Form.Control
                         as="select"
@@ -152,10 +153,9 @@ const Cart = () => {
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 items
               </h2>
-              $
-              {cartItems
-                .reduce((acc, item) => acc + item.qty * item.price, 0)
-                .toFixed(2)}
+              {formatPrice(
+                cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)
+              )}
             </ListGroup.Item>
             <ListGroup.Item>
               <Button
